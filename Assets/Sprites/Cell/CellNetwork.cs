a1fe7706026dd0292a025a9ca3536d8935f21a8e
Corpyr <59375258+LemonAndMint@users.noreply.github.com>
2025-02-24 00:14:22 +0300

    Added ternary tree for store cells

---
+++ Assets/Sprites/Cell/CellNetwork.cs
@@ -0,0 +1,112 @@
+using System;
+using UnityEngine;
+
+namespace CellNetwork
+{
+    public class CellNetwork : MonoBehaviour
+    {
+        public CellNode MainCellNode;
+
+        //We will add the "cells" based on their bounding order. Which cell is bounded first would be
+        //the nearest left node. 
+        public void Add(int currCellID, CellStats cellToBeBoundStat, float boundLength){
+
+            CellNode? targetCellNode = _searchRecursive(MainCellNode, currCellID);
+
+            if(targetCellNode != null){
+
+                targetCellNode.Add(cellToBeBoundStat, boundLength);
+
+            }
+
+        }
+
+        private CellNode _searchRecursive(CellNode currNode, int targetCellID){
+
+            for (int i = 0; i < currNode.Nodes.Length; i++)
+            {
+                
+                if(currNode.Nodes[i] != null && currNode.Nodes[i]?.NextNode.Stats.ID != targetCellID){
+
+                    _searchRecursive(currNode.Nodes[i]?.NextNode, targetCellID);
+
+                }
+                else{
+
+                    return currNode.Nodes[i]?.NextNode;
+
+                }
+
+            }
+
+            new ArgumentNullException("currNode", "Couldnt find the target Cell!");
+            return null; //normally it shouldnt be null.
+
+        }
+
+    }
+
+    public class CellNode{
+
+        public Bound?[] Nodes = new Bound?[3]; // based on ternary tree
+        public CellStats Stats;
+
+        public CellNode(CellStats cellStats){
+
+            Stats = cellStats;
+
+        }
+
+        public void Add(CellStats cellToBeBoundStat, float boundLength){
+
+            int unOccipiedIndex = GetNearestEmptyIndex();
+
+            if(unOccipiedIndex != -1){
+
+                CellNode boundedCell = new CellNode(cellToBeBoundStat);
+                BoundStats boundStats = new BoundStats(boundLength);
+
+                Bound bound = new Bound { 
+
+                    NextNode = boundedCell,
+                    BoundStats = boundStats
+
+                };
+
+                Nodes[unOccipiedIndex] = bound;
+
+            }
+
+            
+            
+        }
+
+        /// <summary>
+        /// </summary>
+        /// <returns>"-1" if all Nodes are occupied.</returns>
+        public int GetNearestEmptyIndex(){
+
+            for (int i = 0; i < Nodes.Length; i++)
+            {
+                
+                if(Nodes[i] == null){
+
+                    return i;
+
+                }
+            }
+
+            return -1;
+
+        }
+
+    }
+
+    public struct Bound{
+
+        public CellNode NextNode;
+        public BoundStats BoundStats;
+
+    }
+
+}
--- Assets/Sprites/Cell/Stats.cs
+++ Assets/Sprites/Cell/Stats.cs
@@ -1,12 +1,31 @@
 using UnityEngine;
 
-namespace Cell.Stats
+public class CellStats
 {
-    
-    public class Stats
-    {
-        public float MoveSpeed;
-        
+    public int ID; //GUID
+    public float Mass; //Different from rigidbody mass.
+    public float MoveSpeed;
+    public Vector3 MoveVector;
+
+    public CellStats(){
+
+        //Generate unique GUID for the cell.
+        //Generate random Mass, MoveSpeed and MoveVector for the object.
+
+    }
+
+}
+
+public class BoundStats
+{
+    public float Length;
+    public float MaxBreakingForce;
+
+    public BoundStats(float Length){
+
+        //Generate random MaxBreakingForce for the bound and according to that change the sprite ccolor.
+        //Length will be based on actual bond length so it wont be random.
+
     }
 
 }
